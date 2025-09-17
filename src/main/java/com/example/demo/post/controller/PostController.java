d65c7086dd188c363f188e4e9d46e87f92c536d7
한도현 <rjsdnd0278@gmail.com>
2025-09-17 23:38:35 +0900

    기존 상태의 코드들

--- src/main/java/com/example/demo/post/controller/PostController.java
+++ src/main/java/com/example/demo/post/controller/PostController.java
@@ -3,18 +3,9 @@ package com.example.demo.post.controller;
 import com.example.demo.post.dto.PostRequestDTO;
 import com.example.demo.post.entity.Post;
 import com.example.demo.post.service.PostService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.ZonedDateTime;
 
 @RestController//Rest가 붙기에 자동으로 @Responsebody 어노테이션 적용됨
 @RequiredArgsConstructor
